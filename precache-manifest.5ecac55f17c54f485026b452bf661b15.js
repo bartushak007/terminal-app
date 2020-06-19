084b179ae691e45ff440a5728b9d1c455c7e28d2
Roman <rmnbrtshk@gmail.com>
2020-06-19 21:55:57 +0300

    Updates

---
+++ precache-manifest.5ecac55f17c54f485026b452bf661b15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80695550a4fc45e8a11ce365997258bc",
+    "url": "/terminal-app/index.html"
+  },
+  {
+    "revision": "db982b571f2fd0d6baaf",
+    "url": "/terminal-app/static/css/main.744e4c9d.chunk.css"
+  },
+  {
+    "revision": "c7322b3f7ebcdf6cd08a",
+    "url": "/terminal-app/static/js/2.76a4d376.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/terminal-app/static/js/2.76a4d376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db982b571f2fd0d6baaf",
+    "url": "/terminal-app/static/js/main.7e647f84.chunk.js"
+  },
+  {
+    "revision": "6af352ae71c37064d5f8",
+    "url": "/terminal-app/static/js/runtime-main.5cb29f41.js"
+  }
+]);
